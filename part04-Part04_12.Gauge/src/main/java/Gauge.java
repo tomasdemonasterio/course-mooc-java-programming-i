a85da6f760a28fecb35c7d0e4822083511951ddd
Tomás de Monasterio <38118995+tomasdemonasterio@users.noreply.github.com>
2024-01-26 10:48:21 -0300

    Add files via upload

---
+++ part04-Part04_12.Gauge/src/main/java/Gauge.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+/**
+ *
+ * @author Set
+ */
+public class Gauge {
+    private int value = 0;
+    
+    public void increase(){
+        if(this.value + 1 <= 5) {
+            this.value++;
+        }
+    }
+    public void decrease() {
+        if(this.value - 1 >= 0) {
+            this.value--;
+        } 
+    }
+    public int value(){
+        return this.value;
+    }
+    public boolean full(){
+        if(this.value == 5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+ 
